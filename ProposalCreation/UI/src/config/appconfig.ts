d7adc55df35bcc3ccc632a2841f31b26f2a774ff
nicolas <nicolas@agilesight.com>
2018-07-18 11:31:36 -0300

    Remove client id

--- ProposalCreation/UI/src/config/appconfig.ts
+++ ProposalCreation/UI/src/config/appconfig.ts
@@ -1,5 +1,5 @@
 export class AppConfig {
-    private static ApplicationId: string = "dd3e88ba-f7e7-423a-bfb6-e63f44286439";
+    private static ApplicationId: string = "<CLIENT_ID>";
     static get applicationId(): string { return AppConfig.ApplicationId; }
     static get accessTokenKey(): string { return "webapiAccessToken"; }
     static get title(): string { return "Commercial Lending"; }
